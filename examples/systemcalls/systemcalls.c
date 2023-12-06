aede7dd5c33a4406da8924ada5b0ca8c550c22b6
Roman kovrigin <RomaKovrigin@gmail.com>
2023-12-06 12:30:27 +0100

    assignment 3, part 1

--- examples/systemcalls/systemcalls.c
+++ examples/systemcalls/systemcalls.c
@@ -16,8 +16,10 @@ bool do_system(const char *cmd)
  *   and return a boolean true if the system() call completed with success
  *   or false() if it returned a failure
 */
+	if (system(cmd) == 0)
+		return true;
 
-    return true;
+    return false;
 }
 
 /**
@@ -58,6 +60,26 @@ bool do_exec(int count, ...)
  *   as second argument to the execv() command.
  *
 */
+	int pid = fork();
+	if (pid == -1)
+		return false;
+
+	if (pid == 0)
+	{
+		// this is a child process
+		int chret;
+		if ((chret = execv(command[0], command)) < 0)
+		{
+			perror("execv %s error\n", command[0]);
+			return chret;
+		}
+		exit(0);
+	}
+	// this is a parent process
+	if (waitpid(pid, &status, 0) == -1)
+	{
+		return -1;
+	}
 
     va_end(args);
 
@@ -92,6 +114,30 @@ bool do_exec_redirect(const char *outputfile, int count, ...)
  *   The rest of the behaviour is same as do_exec()
  *
 */
+	int fd = open(outputfile, O_WRONLY|O_TRUNC|O_CREAT, 0644);
+	if (fd < 0)
+	{
+		perror("open file");
+		abort();
+	}
+	pid = fork();
+	switch (pid)
+	{
+		case -1: perror("fork"); exit(-1);
+				 break;
+		case 0: //child
+		        if (dup2(fd, 1) < 0) { perror("dup2"); exit(-1); }
+				close(fd);
+				if (execv(command[0], command) < 0) {perror("error %s exec", command[0]); exit(-1);}
+		default:
+				close(fd);
+				break;
+	}
+	// this is a parent process
+	if (waitpid(pid, &status, 0) == -1)
+	{
+		return -1;
+	}
 
     va_end(args);
 
